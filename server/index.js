71f9c2e207ccc939232ca8bf1a31c8cd63741b39
Darragh Ferry <darraghferry@hotmail.com>
2020-08-14 17:53:23 +0100

    socket implementation

--- server/index.js
+++ server/index.js
@@ -12,6 +12,15 @@ const io = socketio(server);
 
 io.on("connection", socket => {
   console.log("We have a connection!");
+
+  socket.on("join", ({ name, room }, callback) => {
+    console.log(name, room);
+    const error = true;
+    // if (error) {
+    //   callback({ error: "error" });
+    // }
+  });
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });

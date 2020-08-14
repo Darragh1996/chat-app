39fa11f59f7e79e5abf101285401c652f4b184c4
Darragh Ferry <darraghferry@hotmail.com>
2020-08-14 16:36:28 +0100

    fixed typo

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,12 @@ import Join from "./components/Join";
 import Chat from "./components/Chat";
 
 const App = () => {
-  <Router>
-    <Route path="/" exact component={Join} />
-    <Route path="/chat" component={Chat} />
-  </Router>;
+  return (
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
+  );
 };
 
 export default App;
